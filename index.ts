b7e587f1f6d9c6f5450dedee32b296af402da13f
tekofx <tekofxx@gmail.com>
2023-10-18 18:09:00 +0200

    Changed help command

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
 import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.TOKEN);
+const commands = [
+  { command: "start", description: "Inicia el bot" },
+  { command: "fox", description: "Envia imagen de un zorro" }
+]
 
 bot.start((ctx) => {
-  ctx.reply('Welcome to my bot!');
+  ctx.reply("Hola soy Pangolin, pulsa en menú para ver mis comandos!");
 });
 
 bot.help((ctx) => {
-  ctx.reply('This is a help message.');
-});
+  const text = commands.map(comando => `/${comando.command}: ${comando.description}`).join("\n");
+  ctx.reply(text);
+})
+
+bot.on("text", (ctx) => {
+  const comandoEncontrado = commands.find(comando => comando.command === ctx.message.text);
+  if (!comandoEncontrado) {
+    ctx.reply("Este comando no existe, usa /help para ver mis comandos");
+  }
+})
 
 bot.command('hello', (ctx) => {
   ctx.reply('Hello!');
@@ -20,10 +32,7 @@ bot.command('fox', async (ctx) => {
   ctx.reply("https://api.tinyfox.dev" + buffer.loc);
 });
 
-bot.telegram.setMyCommands([
-  { command: "start", description: "Inicia el bot" },
-  { command: "fox", description: "Envia imagen de un zorro" }
-])
+bot.telegram.setMyCommands(commands);
 
 bot.launch();
 console.log("Running bot")
