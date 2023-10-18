2a539b9a823726344eb5af4801f2acb888952aa8
tekofx <tekofxx@gmail.com>
2023-10-18 16:03:31 +0200

    Added command fox

--- index.ts
+++ index.ts
@@ -13,10 +13,17 @@ bot.help((ctx) => {
 bot.command('hello', (ctx) => {
   ctx.reply('Hello!');
 });
+bot.command('fox', async (ctx) => {
+  const response = await fetch('https://api.tinyfox.dev/img.json?animal=fox');
+  const buffer = await (response.json());
+  console.log(buffer)
+  ctx.reply("https://api.tinyfox.dev" + buffer.loc);
+});
 
 bot.on('text', (ctx) => {
   ctx.reply(`You said: ${ctx.message.text}`);
 });
+
 
 bot.launch();
 console.log("Running bot")
